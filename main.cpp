561e71328d9143d34f21e4f3857a997787291039
xyf <xu_yanfeng@126.com>
2022-11-01 11:03:08 +0800

    宏定义

--- main.cpp
+++ main.cpp
@@ -5,6 +5,17 @@ int main(int, char **)
 {
     auto lib = new Lib();
     lib->test();
-    std::cout << "Hello, world!\n"
-              << test::getN();
+#ifdef TEST
+    if (TEST == 1)
+    {
+        std::cout << "define TEST = 1";
+    }
+    else
+    {
+
+        std::cout << "define TEST";
+    }
+#else
+    std::cout << "no define TEST";
+#endif
 }
